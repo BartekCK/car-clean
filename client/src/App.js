e94593bd6be25e9a6cfb1dc7e63661a1affd2def
Bartlomiej <bkotarski1@gmail.com>
2020-03-12 11:57:43 +0100

    Init commit with dane.txt, db connect, spring boot and react

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return(<div>
+      <h1>Best group</h1>
+      <ul>
+        <li>Bartłomiej Kotarski</li>
+        <li>Marcin Kroczak</li>
+      </ul>
+    </div>)
+  }
+}
+export default App;
