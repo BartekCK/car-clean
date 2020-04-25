afd4b8b7b047fdf16318d6b77e73891a9bfbac60
Bartlomiej <bkotarski1@gmail.com>
2020-04-25 13:12:53 +0200

    Add switch in app.properties to enable and disable sending mails

--- server/src/main/java/com/carwash/server/services/MailServiceImpl.java
+++ server/src/main/java/com/carwash/server/services/MailServiceImpl.java
@@ -21,41 +21,49 @@ public class MailServiceImpl implements MailService {
     @Value("${spring.mail.username}")
     private String hostMail;
 
+    @Value("${enable.sending.mail}")
+    private boolean isEnable;
+
     @Autowired
     public MailServiceImpl(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
     public ResponseEntity sendMail(MailDto mailDto, boolean isHtmlContent) throws MessagingException {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-        mimeMessageHelper.setTo(hostMail);
-        mimeMessageHelper.setSubject(mailDto.getHeader());
-        mimeMessageHelper.setText(mailDto.getMessage(), isHtmlContent);
-        javaMailSender.send(mimeMessage);
-        return ResponseEntity.ok("Wiadomość została wysłana");
+        if (isEnable) {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setTo(hostMail);
+            mimeMessageHelper.setSubject(mailDto.getHeader());
+            mimeMessageHelper.setText(mailDto.getMessage(), isHtmlContent);
+            javaMailSender.send(mimeMessage);
+            return ResponseEntity.ok("Wiadomość została wysłana");
+        }
+        return ResponseEntity.ok("Serwer poczty jest wyłączony, zmień ustawienia aplikacji");
     }
 
     public void informationAboutServiceStatus(String email, OrderServiceStatus orderServiceStatus) throws MessagingException {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-        mimeMessageHelper.setTo(email);
-        mimeMessageHelper.setSubject("Zmiana statusu serwisu");
-        mimeMessageHelper.setText("Cześć, \nstatus Twojego serwisu został zmieniony na <b>" +
-                orderServiceStatus.toString() + "</b>", true);
-        javaMailSender.send(mimeMessage);
+        if (isEnable) {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setTo(email);
+            mimeMessageHelper.setSubject("Zmiana statusu serwisu");
+            mimeMessageHelper.setText("Cześć, \nstatus Twojego serwisu został zmieniony na <b>" +
+                    orderServiceStatus.toString() + "</b>", true);
+            javaMailSender.send(mimeMessage);
+        }
     }
 
     public void informationAboutRegistration(User user) throws MessagingException {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-        mimeMessageHelper.setTo(user.getEmail());
-        mimeMessageHelper.setSubject("Założyłeś konto na car-clean.pl");
-        mimeMessageHelper.setText("<b>Witamy cię " + user.getUsername() + " w naszym gronie.<br>Już teraz skorzystaj z naszej oferty produktów" +
-                " do czyszczenia pojazdu.<br> Oferujemy również profesjonalny detaling samochdów. Przyjedź do nas już dziś i przekonaj się sam.<br>" +
-                "Samych sukcesów !</b>", true);
-        javaMailSender.send(mimeMessage);
+        if (isEnable) {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setTo(user.getEmail());
+            mimeMessageHelper.setSubject("Założyłeś konto na car-clean.pl");
+            mimeMessageHelper.setText("<b>Witamy cię " + user.getUsername() + " w naszym gronie.<br>Już teraz skorzystaj z naszej oferty produktów" +
+                    " do czyszczenia pojazdu.<br> Oferujemy również profesjonalny detaling samochdów. Przyjedź do nas już dziś i przekonaj się sam.<br>" +
+                    "Samych sukcesów !</b>", true);
+            javaMailSender.send(mimeMessage);
+        }
     }
-
-
 }
